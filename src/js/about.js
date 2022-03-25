4b25c234981500d88e1e19f582ab2773a71b23d2
jennychiaen <jenny830806@gmail.com>
2022-03-25 19:11:00 +0800

    feat 新增about 輪播圖

---
+++ src/js/about.js
@@ -0,0 +1,10 @@
+
+        var swiper = new Swiper(".mySwiper", {
+            slidesPerView: 7, //一頁幾個
+            spaceBetween: 50,  //照片間格
+            loop:true,
+            scrollbar: {
+              el: ".swiper-scrollbar",
+              hide: true,
+            },
+          });
