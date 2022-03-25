39b24bd2909b7c64ee84f2ce0725268a49c84f77
TFD10506 <TFD10506@gmail.com>
2022-03-25 14:28:33 +0800

    fix: 修正登入頁面顯示在畫面的問題，改為點擊登入頭像再顯示登入頁面

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,8 @@ console.log('main');
 let ham_icon = document.getElementById("ham-icon");
 let ham_menu = document.querySelector("ul.menu");
 let ham_mask = document.getElementById("menu-backdrop");
+let login_page_icon = document.getElementById("login_page_icon");
+let login = document.getElementById("login");
 ham_icon.addEventListener("click", function () {
     // 選單收合
     ham_menu.classList.toggle("menu-active");
@@ -20,3 +22,11 @@ ham_mask.addEventListener("click", function () {
     // 遮罩
     ham_mask.classList.toggle('menu-backdrop');
 });
+    // 點擊開啟登入註冊
+    login_page_icon.addEventListener("click", function () {
+        // 選單收合
+        console.log(login);
+        login.classList.add("-login_page_icon");
+    });
+    
+
