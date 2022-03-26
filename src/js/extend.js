14fb2c70ec747b318522cc74d2cc2b1f25bdcf55
SYdni-31 <zemi1211@gmail.com>
2022-03-27 01:10:06 +0800

    fix:修正虛擬商展輪播bug;fate:新增展攤樣式1

--- src/js/extend.js
+++ src/js/extend.js
@@ -1,32 +1,15 @@
 // =================貓頭鷹輪播=================
 var owl = $('.owl-carousel');
+console.log(owl)
 owl.owlCarousel({
-    stagePadding: 50,
+    stagePadding: 20,
     loop:true,
-    margin:5,
+    margin:10,
     nav:true,
     dots:false,
-    autoplay:false,
+    autoplay:true,
     autoplayTimeout:3000,
     autoplayHoverPause: true,
-    responsive:{
-        0:{
-            items:3
-        },
-        768:{
-            items:5
-        },
-        1000:{
-            items:8
-        }
-    }
-})
-// 滑鼠滾動事件
-owl.on('mousewheel', '.owl-stage', function (e) {
-    if (e.deltaY>0) {
-        owl.trigger('next.owl');
-    } else {
-        owl.trigger('prev.owl');
-    }
-    e.preventDefault();
-});
+    autoWidth:true,
+    items:10,
+})    
