a5a0d8e1eec82138d92dacc33d0d931d06edb7a1
TFD10506 <TFD10506@gmail.com>
2022-03-29 13:47:58 +0800

    feat: 修改現正直播的留言板html及css，新增現正直播icon圖片

---
+++ src/js/live.js
@@ -0,0 +1,153 @@
+$(function () {
+  // 影片判斷
+  if ($(".live-viedo").attr("src") == "") {
+    // alert("無直播");
+
+  } else {
+    // alert("有直播");
+  }
+  // 點擊收合留言板
+  $("#live-icon-img").on("click", function () {
+    $("#live-icon-img").toggleClass("-transform-180");
+    $(".live-board-container").toggleClass("-live-on");
+    // 要關閉，快點消失
+    if ($("#live-icon-img").attr("class") == "-transform-180") {
+      setTimeout(live_text, 50);
+      // 要開啟，要慢點出現
+    } else {
+      setTimeout(live_text, 700);
+    }
+  });
+  function live_text() {
+    $("#search-container-title-h3").toggleClass("-hide");
+    $(".input-block").toggleClass("-live-border-hide");
+    $(".search-container-title").toggleClass("-live-border-hide");
+    $("#live-text-input").toggleClass("-hide");
+    $("#live-submit").toggleClass("-hide");
+    $(".search-container").toggleClass("-hide");
+    $(".result-block").toggleClass("live-height");
+  }
+  $("#live-text-input").on("keydown", function (e) {
+    // console.log(e.which);
+    if (e.which == 13) {
+      $("#live-submit").click();
+    }
+  });
+
+  $("#live-submit").on("click", function () {
+    const input_text = $("#live-text-input").val().trim();
+    if (input_text == "") {
+      alert("請輸入資料");
+    } else {
+      let html = `
+          <ul class="search-list">
+            <li class="search-li">
+            <div class="search-li-icon">
+                <i class="fa-solid fa-user"></i>
+            </div>
+            <div>
+              <div class="item-name">
+              王大明
+              </div>
+              <div class="item-block">
+                <div class="title-block">${input_text}</div>
+
+              </div>
+              </div>
+            </li>
+          </ul>
+        `;
+
+      $("div.search-container").append(html);
+
+      // 重點1：以下方式取得 section.result-block(藍框) 的可捲動區域高度(不含邊框)
+      //console.log( $('section.result-block').prop("scrollHeight") );
+
+      // 重點2：透過 animate 使用 scrollTop 指定要滑動到離上方指定位置
+      $(".result-block").animate(
+        {
+          //scrollTop: 10
+          scrollTop: $(".result-block").prop("scrollHeight"),
+        },
+        10
+      );
+
+
+
+    }
+
+    // // 上課測試
+    // const input = $("#live-text-input");
+    // // 在這裡改成php網址
+    // // const url = "https://jsonplaceholder.typicode.com/todos/";
+    // const url = "http://10.2.0.84:8080/spring-exercise/test/login";
+    // // fetch() 會回傳Promise物件
+    // fetch(`${url}${input.val()}`)
+    // // .then(自己取的名字 => 自己取的名字.json())
+    //   .then(resp => resp.json())
+    //   .then(body => console.log(body));
+    // ;
+
+
+    // 第二個測試
+    // const url = "login.php";
+
+    // const username = $("#username");
+    // const password = $("#password");
+    // fetch(url, {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json'
+    //   },
+    //   body: JSON.stringify({
+    //     username: $("#username").val(),
+    //     password: $("#password").val()
+    //   })
+    // })
+    //   .then(resp => resp.json())
+    //   .then(body => {
+    //     const { successful, NICKNAME } = body;
+    //     // div.textContent = successful ? nickname : message;
+    //     if (successful) {
+    //       $(".test-div").text(NICKNAME);
+    //     } else {
+    //       $(".test-div").text("");
+    //     }
+    //   });
+
+
+
+    // // 測試欄位
+    // $("#username").val("");
+    // $("#password").val("");
+
+    // 上傳圖片
+    // const url = "http://10.2.0.84:8080/spring-exercise/test/fileUpload";
+    // const file = $("#file-test")[0].files[0];
+    // const fileReader = new FileReader();
+    // fileReader.onload = function (event) {
+    //     const base64Str = btoa(event.target.result);
+    //     fetch(url, {
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //         },
+    //         body: JSON.stringify({
+    //             fileName: file.name,
+    //             file: base64Str
+    //         })
+    //     })
+    //         .then(resp => resp.json())
+    //         .then(body => {
+    //             const { successful ,message} = body;
+    //             if(successful){
+    //                 alert(message);
+    //             }
+    //         });
+
+    // }
+    // fileReader.readAsBinaryString(file);
+    // 欄位清空
+    $("#live-text-input").val("");
+  });
+});
--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,3 @@
-console.log('main');
 // ------ Hamburger漢堡選單 ------
 let ham_icon = document.getElementById("ham-icon");
 let ham_menu = document.querySelector("ul.menu");
@@ -6,26 +5,24 @@ let ham_mask = document.getElementById("menu-backdrop");
 let login_page_icon = document.getElementById("login_page_icon");
 let login = document.getElementById("login");
 ham_icon.addEventListener("click", function () {
-    // 選單收合
-    ham_menu.classList.toggle("menu-active");
-    // 漢堡按鈕特效
-    ham_icon.classList.toggle('open');
-    // 遮罩
-    ham_mask.classList.toggle('menu-backdrop');
+  // 選單收合
+  ham_menu.classList.toggle("menu-active");
+  // 漢堡按鈕特效
+  ham_icon.classList.toggle("open");
+  // 遮罩
+  ham_mask.classList.toggle("menu-backdrop");
 });
-    // 點擊遮罩
+// 點擊遮罩
 ham_mask.addEventListener("click", function () {
-    // 選單收合
-    ham_menu.classList.toggle("menu-active");
-    // 漢堡按鈕特效
-    ham_icon.classList.toggle('open');
-    // 遮罩
-    ham_mask.classList.toggle('menu-backdrop');
+  // 選單收合
+  ham_menu.classList.toggle("menu-active");
+  // 漢堡按鈕特效
+  ham_icon.classList.toggle("open");
+  // 遮罩
+  ham_mask.classList.toggle("menu-backdrop");
 });
-    // 點擊開啟登入註冊
-    login_page_icon.addEventListener("click", function () {
-        // 選單收合
-        login.classList.add("-login_page_icon");
-    });
-    
-
+// 點擊開啟登入註冊
+login_page_icon.addEventListener("click", function () {
+  // 選單收合
+  login.classList.add("-login_page_icon");
+});
