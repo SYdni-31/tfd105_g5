c22debe7de84b4cd2a389bc15cfcec3618958dee
SYdni-31 <zemi1211@gmail.com>
2022-03-29 19:17:50 +0800

    feat: 完成攤位整體排版與輪播、新增border外框class in typography、新增padding&margin四面的class in_space

--- src/js/extend.js
+++ src/js/extend.js
@@ -1,7 +1,6 @@
 // =================輪播=================  
 var swiper = new Swiper(".mySwiper", {
     loop: true,
-    autoHeight: false,
     spaceBetween: 20,
     slidesPerView: "auto",
     freeMode: true,
@@ -18,3 +17,22 @@ var swiper = new Swiper(".mySwiper", {
       swiper: swiper,
     },
   }); 
+  // =================頁面切換=================  
+  window.addEventListener('DOMContentLoaded', function(){
+    let content_point= document.querySelectorAll('.content_point')
+    let content_inner= document.querySelectorAll('.content_inner')
+    console.log(content_inner)
+    console.log(content_point)
+    content_point.forEach(function(item, index){
+      item.addEventListener('click', function(){
+        let others_point=this.parentElement.querySelectorAll('.content_point')
+        let others_content=this.parentElement.querySelectorAll('.content_inner')
+        for(let i = 0; i < others_content.length; i++){
+          others_point[i].classList.remove("content_pointed");
+          others_content[i].classList.add("-hide");
+        }
+        this.classList.add("content_pointed")
+        content_inner[index].classList.remove("-hide")
+      })
+    })
+  })
