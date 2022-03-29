0b231d05668756b9f15f9aebd297237e1d64b7f6
gleemarc <marc810824@gmail.com>
2022-03-29 19:59:00 +0800

    feat:調整聚光燈顏色/新增輪播套件樣式/待處理:將分項文字塞進去img裡面/

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+// 輪播event coverflow
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 60,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
