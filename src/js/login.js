84139deaedc72ee43c3be4418f8425e0df7f6906
hsiang119 <g8367ay36@gmail.com>
2022-03-29 16:32:59 +0800

    fix: 修補廠商登入區塊沒有背景色問題 , 修補登入頁燈箱關閉後首頁無法滑動問題

--- src/js/login.js
+++ src/js/login.js
@@ -2,11 +2,13 @@ document.addEventListener("DOMContentLoaded", function () {
     let login_page_icon = document.getElementById('login_page_icon');
     let login = document.getElementById('login');
     let btn_close = document.getElementById('BtnClose');
+    //點擊人頭叫出登入頁燈箱
     login_page_icon.addEventListener('click', function () {
         login.classList.remove('js-none');
         login.classList.add('js-block');
-        // document.body.style.overflow = 'hidden';
+        document.body.style.overflow = 'hidden';  
     });
+    //點擊叉叉可以關閉燈箱
     btn_close.addEventListener('click', function () {
         CloseLightBox();
     });
@@ -15,7 +17,7 @@ document.addEventListener("DOMContentLoaded", function () {
 function CloseLightBox() {
     login.classList.remove('-login_page_icon');
     login.classList.remove('js-block');
-    document.body.style.overflow = 'show';  
+    document.body.style.overflow = '';  
 }
 
 //點擊遮罩處可以關閉燈箱
@@ -25,7 +27,7 @@ $(document).on("click", "form.login", function(e) {
     }
 });
 
-//按Esc可以關閉燈箱
+//按Esc鍵可以關閉燈箱
 $(document).keyup(function(e) {
     if (e.which == '27') {
         CloseLightBox();
