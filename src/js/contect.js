89f9b25b72e00fed322b69ef668f6fb5e1ed9749
jennychiaen <jenny830806@gmail.com>
2022-03-30 12:00:32 +0800

    feat 修改contect QA html 改成以flex方式排版,js 改成為符號轉180度及增加當按下時改顏色

--- src/js/contect.js
+++ src/js/contect.js
@@ -1,5 +1,11 @@
-$(".contect_btn").on("click", function(e){
+$(".contect_question").on("click", function(e){
     $(this).closest("li").toggleClass("-on");
     $(this).closest("li").find("div.contect_answer").slideToggle();
+    //展開符號180度選轉
+    $(this).closest("li").find(".fa-chevron-down").toggleClass("-transform-180");
+    //展開標題改顏色
+    $(this).closest("li").find(".contect_question").toggleClass("-change-color");
+    //展開符號改顏色
+    $(this).closest("li").find(".fa-chevron-down").toggleClass("-change-color");
   });
 
