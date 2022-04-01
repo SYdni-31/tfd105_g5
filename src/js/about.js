647b001353f683f84ec5ae34af96091b497686f2
jennychiaen <jenny830806@gmail.com>
2022-04-01 11:57:39 +0800

    feat about頁面格式調整

--- src/js/about.js
+++ src/js/about.js
@@ -1,30 +1,35 @@
 
-        var swiper = new Swiper(".mySwiper", {
-            slidesPerView: 7, //一頁幾個
-            spaceBetween: 50,  //照片間格
-            loop:true,
-            scrollbar: {
-              el: ".swiper-scrollbar",
-              hide: false,
-              draggable:true,
-        
-            },
-            breakpoints:{
-              375:{
-                slidesPerView: 4,
-                spaceBetween: 20,
-              },
-              480:{
-                slidesPerView: 4,
-                spaceBetween: 20,
-              },
-              768:{
-                slidesPerView: 5,
-                spaceBetween: 20,
-              },
-              1024: {
-                slidesPerView: 7,
-                spaceBetween: 50,
-              },
-            }
-          });
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 7, //一頁幾個
+  spaceBetween: 50,  //照片間格
+  loop: true,
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    hide: false,
+    draggable: true,
+
+  },
+  breakpoints: {
+    375: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+    480: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 5,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 7,
+      spaceBetween: 50,
+    },
+  }
+});
+
+var speaker = document.getElementsByClassName("about_speaker_intro");
+for(let i = 0; i < speaker.length; i++){
+  
+}
