527634b929fd877ed8b1313f443f50e526e0eb51
SYdni-31 <zemi1211@gmail.com>
2022-04-06 16:54:35 +0800

    feat: 完成vue結合datatable與彈窗in backstage_info1

--- src/js/backstage_info1.js
+++ src/js/backstage_info1.js
@@ -1,6 +1,93 @@
 // =====datdtable=====
+
 $(document).ready( function () {
+    Vue.component('backstage_info1_edit',{
+        props:['data'],
+        data(){
+            return{
+                redio:this.data[3],
+            }
+        },
+        methods: {
+            f_save(){
+                this.$swal({
+                    title: "儲存成功",
+                    icon: "success",
+                    image: "",
+                  }).then((willInsert) => {
+                      this.$emit('close',null)
+                    })
+            },
+            f_close(){
+                this.$swal({
+                    title: "尚未存檔，是否關閉?",
+                    icon: "warning",
+                    buttons: true,
+                    dangerMode: true,
+                  }).then((willInsert) => {
+                      if(willInsert){
+                        this.$emit('close',null)
+                      }
+                  })
+            },
+        },
+        template:`
+        <article class="backstage_box backstage_info1_edit">
+                <h2>修改</h2>
+                <div class="backstage_box-content pt-30">
+                    <ul>
+                        <li class="mb-16 input-short"><label for="name">策展ID</label>
+                            <input type="text" name="name" id="name" :value="data[0]" disabled>
+                        </li>
+                        <li class="mb-16 input-short"><label for="introduce">展會名稱</label>
+                            <input type="text" name="introduce" id="introduce" :value="data[1]">
+                        </li>
+                        <li class="mb-16 input-long"><label for="time">會議簡介</label>
+                            <textarea name="time" id="time" cols="30" rows="10" :value="data[4]"></textarea>
+                        <li class="mb-16 input-short"><label for="startdate">活動開始</label>
+                            <input type="date" name="date" id="startdate"  :value="data[2]">
+                        </li>
+                        <li class="mb-16 input-short"><label for="enddate">活動結束</label>
+                            <input type="date" name="date" id="enddate"  :value="data[5]">
+                        </li>
+                        <div class="mb-16 input-short"><label>進行狀態</label><br>
+                            <label for="notwork"><input type="radio" name="date" id="notwork"  value="尚未開始" v-model="redio">尚未開始</label>
+                            <label for="working"><input type="radio" name="date" id="working"  value="進行中" v-model="redio">進行中</label>
+                            <label for="worked"><input type="radio" name="date" id="worked"  value="已結束" v-model="redio">已結束</label>
+                        </div>
+                    </ul>                   
+                    <div class="back-insert-btn">
+                        <button class="back-insert_save" @click="f_save">儲存</button>
+                        <button class="back-insert_close" @click="f_close">關閉</button>
+                    </div>
+                </div>
+            </article>`,
+        
+    })
     Vue.component('backstage_info1_add',{
+        methods: {
+            f_save(){
+                this.$swal({
+                    title: "儲存成功",
+                    icon: "success",
+                    image: "",
+                }).then((willInsert) => {
+                    this.$emit('close',null)
+                })
+            },
+            f_close(){
+                this.$swal({
+                    title: "尚未存檔，是否關閉?",
+                    icon: "warning",
+                    buttons: true,
+                    dangerMode: true,
+                  }).then((willInsert) => {
+                      if(willInsert){
+                        this.$emit('close',null)
+                      }
+                  })
+            },
+        },
         template:`
         <article class="backstage_box backstage_info1_add">
                 <h2>新增</h2>
@@ -21,14 +108,14 @@ $(document).ready( function () {
                             <input type="date" name="date" id="enddate">
                         </li>
                         <div class="mb-16 input-short"><label>進行狀態</label><br>
-                            <label for="notwork"><input type="radio" name="date" id="notwork">尚未開始</label>
-                            <label for="working"><input type="radio" name="date" id="working">進行中</label>
-                            <label for="worked"><input type="radio" name="date" id="worked">已結束</label>
+                            <label for="notwork"><input type="radio" name="date" id="notwork" value="尚未開始">尚未開始</label>
+                            <label for="working"><input type="radio" name="date" id="working" value="進行中">進行中</label>
+                            <label for="worked"><input type="radio" name="date" id="worked" value="已結束">已結束</label>
                         </div>
                     </ul>                   
                     <div class="back-insert-btn">
-                        <button class="back-insert_save">儲存</button>
-                        <button class="back-insert_close">關閉</button>
+                        <button class="back-insert_save" @click="f_save">儲存</button>
+                        <button class="back-insert_close" @click="f_close">關閉</button>
                     </div>
                 </div>
             </article>`,
@@ -37,20 +124,26 @@ $(document).ready( function () {
     Vue.component('datatable',{
         data(){
             return{
-                box:null,
+                box_add:null,
+                row_data:null,
             }
         },
         methods:{
+            edit(data){
+                this.row_data=data
+                this.box_add="backstage_info1_edit"
+            }
         },
         template:`
         <article class="-margin0auto pt-10 table_outer">
-        <button @click="box='backstage_info1_add'" class="backstage_add backstage_btn backstage_btn_add mb-15">新增</button>
+        <button @click="box_add='backstage_info1_add'" class="backstage_add backstage_btn backstage_btn_add mb-15">新增</button>
         <h3 class="bg-color pall-15">講師介紹</h3>
         <table id="table" class="table"></table>
-        <component :is="box"></component>
+        <component :is="box_add" @close="box_add=null" :data="row_data"></component>
         </article>`,
         mounted: function () {
-            $('#table').DataTable({
+            let vm = this
+            let table=$('#table').DataTable({
                 "lengthMenu": [10, 20],
                 "dom": `<'row'<'col-6'l><'col-6'f>>
                         <'row'<'col-12'tr>>
@@ -80,32 +173,71 @@ $(document).ready( function () {
                     [
                         1,
                         "Tián lám 2022智慧全球科技聚策展",
-                        "2022/03/17",
+                        "2022-03-17",
                         "進行中",
-                        `<div class="backstage_btn_td"><button class="backstage_btn backstage_btn_short">修改</button><button class="backstage_btn backstage_btn_bad ml-4">刪除</button></div>`,
+                        "會議簡介1",
+                        "2022-03-20",
+                        // `<div class="backstage_btn_td"><button class="backstage_btn backstage_btn_short">修改</button><button class="backstage_btn backstage_btn_bad ml-4">刪除</button></div>`,
                         
                     ],
                     [
                         2,
+                        "想不出名字的展會",
+                        "2022-03-19",
+                        "進行中",
+                        "會議簡介2",
+                        "2022-03-20",
+                        // `<div class="backstage_btn_td"><button class="backstage_btn backstage_btn_short">修改</button><button class="backstage_btn backstage_btn_bad ml-4">刪除</button></div>`,
+                        
+                    ],
+                    [
+                        3,
                         "台北科技展",
-                        "2022/03/18",
+                        "2022-03-18",
                         "已結束",
-                        `<div class="backstage_btn_td"><button class="backstage_btn backstage_btn_short">修改</button><button class="backstage_btn backstage_btn_bad ml-4">刪除</button></div>`,
+                        "會議簡介3",
+                        "2022-03-20",
+                        // `<div class="backstage_btn_td"><button class="backstage_btn backstage_btn_short">修改</button><button class="backstage_btn backstage_btn_bad ml-4">刪除</button></div>`,
                         
                     ]],
                 "columns": [
-                  { title: "策展" },
-                  { title: "策展名稱" },
-                  { title: "活動開始" },
-                  { title: "狀態" },
-                  { title: "操作" },
+                    { title: "策展" },
+                    { title: "策展名稱" },
+                    { title: "活動開始" },
+                    { title: "狀態" },
+                    {title: "操作",
+                        render: function (data, type, row) {
+                            return (
+                                `<div class="backstage_btn_td">
+                                <button type="button" class="backstage_btn backstage_btn_short edt_btn">編輯</button>
+                                <button type="button" class="backstage_btn backstage_btn_bad ml-4 del_btn">刪除</button>
+                                </div`
+                            );
+                        },
+                    },
                 ]
             });
             // 拉動頁面時的
             $(window).on('resize', function(){
                 location.reload()
             })
-          }
+            // 修改
+            $("#table tbody").on("click", ".edt_btn", function () {
+                var data = table.row($(this).parents("tr")).data();
+                // data[0]是第1筆以此類推
+                vm.edit(data)
+            });
+            // 刪除
+            $("#table tbody").on("click", ".del_btn", function () {
+                swal({
+                    title: "是否確定刪除?",
+                    icon: "warning",
+                    buttons: true,
+                    dangerMode: true,
+                })
+            })
+              
+        }
     })
     
     new Vue({
@@ -113,7 +245,6 @@ $(document).ready( function () {
         data() {
             return {
                 aside_btn: false,
-                box: null,
             };
           },
         methods: {
