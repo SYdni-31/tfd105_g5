ee18a4b367ba636a543a10c0229a377da5810d6a
gleemarc <marc810824@gmail.com>
2022-04-07 17:46:18 +0800

    feat:更改彈窗設定，刪除空值scss

--- src/js/c_back.js
+++ src/js/c_back.js
@@ -6,6 +6,9 @@ for(let i=0; i < btnOpenChat.length; i++){
     // console.log(btnOpenChat[i]);
     btnOpenChat[i].addEventListener('click', ()=>{
         document.getElementsByClassName('two')[0].setAttribute("style", "display:block");
+        // document.getElementsByClassName('two')[0].classList.toggle("c_bank_mask");
+        
+        // document.body.style.overflow = "hidden";
     })
 }
 
@@ -14,8 +17,14 @@ let btnCloseChat = document.getElementById('c_back_close');
 btnCloseChat.addEventListener('click',()=>{
     // console.log(BtnClose);
     closeChatbox();
+    document.body.style.overflow = "";
 })
 
+
+// 函式
 function closeChatbox () {
     document.getElementsByClassName('two')[0].setAttribute("style", "");
 }
+// function closeLightbox() {
+//     document.getElementsByClassName('c_bank_mask')[0].
+// }
