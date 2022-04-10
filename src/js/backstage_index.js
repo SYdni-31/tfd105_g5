ecc29c34a1d095ae7a2eca5466bb074b729be358
SYdni-31 <zemi1211@gmail.com>
2022-04-11 02:09:58 +0800

    feat:新增後台連接資料庫的update與delete功能

--- src/js/backstage_index.js
+++ src/js/backstage_index.js
@@ -320,15 +320,63 @@
         },
         methods: {
             f_save(){
-                this.$swal({
-                    title: "儲存成功",
-                    icon: "success",
-                    image: "",
-                }).then((willInsert) => {
-                    if(willInsert){
-                        this.$emit('editsave', this.newdata)
+                if(this.newdata.NAME && this.newdata.NAME !=""
+                && this.newdata.START_TIME && this.newdata.START_TIME !=""
+                && this.newdata.END_TIME && this.newdata.END_TIME !=""
+                && this.newdata.OPEN && this.newdata.OPEN !=""
+                && this.newdata.INTRODUCE && this.newdata.INTRODUCE !=""){
+                // 確認所有欄位是否都有值
+                    // 確認開始日期是否小於結束日期
+                    let starttime= (this.newdata.START_TIME).split('-').join('')
+                    let endtime= (this.newdata.END_TIME).split('-').join('')
+                    if(starttime<=endtime){
+                        console.log(starttime, endtime)
+                        fetch('php/backstage_info1_update_expo.php', {
+                            method: 'POST',
+                            headers:{
+                                'Content-Type': 'application/json'
+                            },
+                            body:JSON.stringify({
+                                ID:this.newdata.ID,
+                                NAME:this.newdata.NAME,
+                                START_TIME:this.newdata.START_TIME,
+                                END_TIME:this.newdata.END_TIME,
+                                OPEN:this.newdata.OPEN,
+                                INTRODUCE:this.newdata.INTRODUCE,
+                            })
+                        }).then(resp =>resp.json())
+                        .then(body =>{
+                            let {successful} =body
+                            if(successful){
+                                this.$swal({
+                                    title: "儲存成功",
+                                    icon: "success",
+                                    image: "",
+                                }).then((willInsert) => {
+                                    this.$emit('addsave')
+                                })
+                            }else{
+                                this.$swal({
+                                    title: "儲存失敗",
+                                    icon: "error",
+                                    text: "請檢查欄位",
+                                });
+                            } 
+                        })
+                    }else{
+                        this.$swal({
+                            title: "儲存失敗",
+                            icon: "error",
+                            text: "請確認日期是否正確",
+                        });
                     }
-                })
+                }else{
+                    this.$swal({
+                        title: "儲存失敗",
+                        icon: "error",
+                        text: "所有欄位皆須填寫",
+                    });
+                }
             },
             f_close(){
                 this.$swal({
@@ -345,39 +393,39 @@
         },
         template:`
         <article class="backstage_box">
-                <h2>修改<i @click="f_close" class="fa-regular fa-circle-xmark backstage_close_icon"></i></h2>
-                <div class="backstage_box-content pt-30">
-                    <ul>
-                        <li class="mb-16 input-short"><label for="id">策展ID</label>
-                            <input type="text" name="id" id="id" v-model="newdata[0]" disabled>
-                        </li>
-                        <li class="mb-16 input-short"><label for="name">展會名稱</label>
-                            <input type="text" name="name" id="name" v-model="newdata[1]">
-                        </li>
-                        <li class="mb-16 input-long"><label for="introduce">會議簡介</label>
-                            <textarea name="introduce" id="introduce" cols="30" rows="10" v-model="newdata[5]"></textarea>
-                        </li>
-                        <li class="mb-16 input-short"><label for="starttime">活動開始</label>
-                            <input type="date" name="starttime" id="starttime" v-model="newdata[2]">
-                        </li>
-                        <li class="mb-16 input-short"><label for="endtime">活動結束</label>
-                            <input type="date" name="endtime" id="endtime" v-model="newdata[3]">
-                        </li>
-                        <div class="mb-16"><label>進行狀態</label><br>
-                            <label for="notwork"><input type="radio" name="open" id="notwork"  value="尚未開始" v-model="newdata[4]">尚未開始</label>
-                            <label for="working"><input type="radio" name="open" id="working"  value="進行中" v-model="newdata[4]">進行中</label>
-                            <label for="worked"><input type="radio" name="open" id="worked"  value="已結束" v-model="newdata[4]">已結束</label>
-                        </div>
-                    </ul>                  
-                    <div class="backstage-insert-btn">
-                        <button class="backstage-insert_save" @click="f_save">儲存</button>
-                        <button class="backstage-insert_close" @click="f_close">關閉</button>
+            <h2>修改<i @click="f_close" class="fa-regular fa-circle-xmark backstage_close_icon"></i></h2>
+            <div class="backstage_box-content pt-30">
+                <ul>
+                    <li class="mb-16 input-short"><label for="ID">策展ID</label>
+                        <input type="text" name="ID" id="ID" v-model="newdata.ID" disabled>
+                    </li>
+                    <li class="mb-16 input-short"><label for="NAME">展會名稱</label>
+                        <input type="text" name="NAME" id="NAME" v-model="newdata.NAME">
+                    </li>
+                    <li class="mb-16 input-long"><label for="INTRODUCR">會議簡介</label>
+                        <textarea name="INTRODUCR" id="INTRODUCR" cols="30" rows="10" v-model="newdata.INTRODUCR"></textarea>
+                    </li>
+                    <li class="mb-16 input-short"><label for="START_TIME">活動開始</label>
+                        <input type="date" name="START_TIME" id="START_TIME" v-model="newdata.START_TIME">
+                    </li>
+                    <li class="mb-16 input-short"><label for="END_TIME">活動結束</label>
+                        <input type="date" name="END_TIME" id="END_TIME" v-model="newdata.END_TIME">
+                    </li>
+                    <div class="mb-16"><label>進行狀態</label><br>
+                        <label for="notwork"><input type="radio" name="OPEN" id="notwork"  value="尚未開始" v-model="newdata.OPEN">尚未開始</label>
+                        <label for="working"><input type="radio" name="OPEN" id="working"  value="進行中" v-model="newdata.OPEN">進行中</label>
+                        <label for="worked"><input type="radio" name="OPEN" id="worked"  value="已結束" v-model="newdata.OPEN">已結束</label>
                     </div>
+                </ul>                  
+                <div class="backstage-insert-btn">
+                    <button class="backstage-insert_save" @click="f_save">儲存</button>
+                    <button class="backstage-insert_close" @click="f_close">關閉</button>
                 </div>
-            </article>`,
+            </div>
+        </article>`,
         created () {
             this.newdata = JSON.parse(JSON.stringify(this.row_data)) 
-            },
+        },
     })
 // ========info1_展覽場次_後台新增按鈕========
     Vue.component('backstage_info1_add',{
@@ -393,43 +441,55 @@
                 && this.newdata.END_TIME && this.newdata.END_TIME !=""
                 && this.newdata.OPEN && this.newdata.OPEN !=""
                 && this.newdata.INTRODUCE && this.newdata.INTRODUCE !=""){
-                    fetch('php/backstage_info1_insert_expo.php', {
-                        method: 'POST',
-                        headers:{
-                            'Content-Type': 'application/json'
-                        },
-                        body:JSON.stringify({
-                            NAME:this.newdata.NAME,
-                            START_TIME:this.newdata.START_TIME,
-                            END_TIME:this.newdata.END_TIME,
-                            OPEN:this.newdata.OPEN,
-                            INTRODUCE:this.newdata.INTRODUCE,
-                        })
-                    }).then(resp =>resp.json())
-                    .then(body =>{
-                        let {successful} =body
-                        if(successful){
-                            this.$swal({
-                                title: "儲存成功",
-                                icon: "success",
-                                image: "",
-                            }).then((willInsert) => {
-                                this.$emit('addsave')
+                // 確認所有欄位是否都有值
+                    // 確認開始日期是否小於結束日期
+                    let starttime= (this.newdata.START_TIME).split('-').join('')
+                    let endtime= (this.newdata.END_TIME).split('-').join('')
+                    if(starttime<=endtime){
+                        fetch('php/backstage_info1_insert_expo.php', {
+                            method: 'POST',
+                            headers:{
+                                'Content-Type': 'application/json'
+                            },
+                            body:JSON.stringify({
+                                NAME:this.newdata.NAME,
+                                START_TIME:this.newdata.START_TIME,
+                                END_TIME:this.newdata.END_TIME,
+                                OPEN:this.newdata.OPEN,
+                                INTRODUCE:this.newdata.INTRODUCE,
                             })
-                        }else{
-                            this.$swal({
-                                title: "儲存失敗",
-                                icon: "error",
-                                text: "請檢查欄位",
-                            });
-                        } 
-                    })
+                        }).then(resp =>resp.json())
+                        .then(body =>{
+                            let {successful} =body
+                            if(successful){
+                                this.$swal({
+                                    title: "儲存成功",
+                                    icon: "success",
+                                    image: "",
+                                }).then((willInsert) => {
+                                    this.$emit('addsave')
+                                })
+                            }else{
+                                this.$swal({
+                                    title: "儲存失敗",
+                                    icon: "error",
+                                    text: "請檢查欄位",
+                                });
+                            } 
+                        })
+                    }else{
+                        this.$swal({
+                            title: "儲存失敗",
+                            icon: "error",
+                            text: "請確認日期是否正確",
+                        });
+                    }
                 }else{
                     this.$swal({
                         title: "儲存失敗",
                         icon: "error",
                         text: "所有欄位皆須填寫",
-                      });
+                    });
                 }
             },
             f_close(){
@@ -447,36 +507,36 @@
         },
         template:`
         <article class="backstage_box">
-                <h2>新增<i @click="f_close" class="fa-regular fa-circle-xmark backstage_close_icon"></i></h2>
-                <div class="backstage_box-content pt-30">
-                    <ul>
-                        <li class="mb-16 input-short"><label for="id">策展ID</label>
-                            <input type="text" name="id" id="id" value="自動編號" disabled>
-                        </li>
-                        <li class="mb-16 input-short"><label for="NAME">展會名稱</label>
-                            <input type="text" name="NAME" id="NAME" v-model.trim="newdata.NAME">
-                        </li>
-                        <li class="mb-16 input-long"><label for="INTRODUCE">會議簡介</label>
-                            <textarea name="INTRODUCE" id="INTRODUCE" cols="30" rows="10" v-model.trim="newdata.INTRODUCE"></textarea>
-                        </li>
-                        <li class="mb-16 input-short"><label for="START_TIME">活動開始</label>
-                            <input type="date" name="START_TIME" id="START_TIME" v-model="newdata.START_TIME">
-                        </li>
-                        <li class="mb-16 input-short"><label for="END_TIME">活動結束</label>
-                            <input type="date" name="END_TIME" id="END_TIME" v-model="newdata.END_TIME">
-                        </li>
-                        <div class="mb-16"><label>進行狀態</label><br>
-                            <label for="notwork"><input type="radio" name="OPEN" id="notwork" value="尚未開始" v-model="newdata.OPEN">尚未開始</label>
-                            <label for="working"><input type="radio" name="OPEN" id="working" value="進行中" v-model="newdata.OPEN">進行中</label>
-                            <label for="worked"><input type="radio" name="OPEN" id="worked" value="已結束" v-model="newdata.OPEN">已結束</label>
-                        </div>
-                    </ul>                   
-                    <div class="backstage-insert-btn">
-                        <button class="backstage-insert_save" @click="f_save">儲存</button>
-                        <button class="backstage-insert_close" @click="f_close">關閉</button>
+            <h2>新增<i @click="f_close" class="fa-regular fa-circle-xmark backstage_close_icon"></i></h2>
+            <div class="backstage_box-content pt-30">
+                <ul>
+                    <li class="mb-16 input-short"><label for="id">策展ID</label>
+                        <input type="text" name="id" id="id" value="自動編號" disabled>
+                    </li>
+                    <li class="mb-16 input-short"><label for="NAME">展會名稱</label>
+                        <input type="text" name="NAME" id="NAME" v-model.trim="newdata.NAME">
+                    </li>
+                    <li class="mb-16 input-long"><label for="INTRODUCE">會議簡介</label>
+                        <textarea name="INTRODUCE" id="INTRODUCE" cols="30" rows="10" v-model.trim="newdata.INTRODUCE"></textarea>
+                    </li>
+                    <li class="mb-16 input-short"><label for="START_TIME">活動開始</label>
+                        <input type="date" name="START_TIME" id="START_TIME" v-model="newdata.START_TIME">
+                    </li>
+                    <li class="mb-16 input-short"><label for="END_TIME">活動結束</label>
+                        <input type="date" name="END_TIME" id="END_TIME" v-model="newdata.END_TIME">
+                    </li>
+                    <div class="mb-16"><label>進行狀態</label><br>
+                        <label for="notwork"><input type="radio" name="OPEN" id="notwork" value="尚未開始" v-model="newdata.OPEN">尚未開始</label>
+                        <label for="working"><input type="radio" name="OPEN" id="working" value="進行中" v-model="newdata.OPEN">進行中</label>
+                        <label for="worked"><input type="radio" name="OPEN" id="worked" value="已結束" v-model="newdata.OPEN">已結束</label>
                     </div>
+                </ul>                   
+                <div class="backstage-insert-btn">
+                    <button class="backstage-insert_save" @click="f_save">儲存</button>
+                    <button class="backstage-insert_close" @click="f_close">關閉</button>
                 </div>
-            </article>`,
+            </div>
+        </article>`,
         
     })
 // ========info1_展覽場次_table========
@@ -505,11 +565,35 @@
                     dangerMode: true,
                 }).then((willDelete) => {
                     if (willDelete) {
-                        this.datas.splice(index, 1)
-                        swal({
-                            title: "刪除成功",
-                            icon: "success",
-                        });
+                        fetch('php/backstage_info1_delete_expo.php', {
+                            method: 'POST',
+                            headers:{
+                                'Content-Type': 'application/json'
+                            },
+                            body:JSON.stringify({
+                                ID:this.datas[index].ID,
+                            })
+                        }).then(resp =>resp.json())
+                        .then(body =>{
+                            let {successful} =body
+                            if(successful){
+                                this.$swal({
+                                    title: "刪除成功",
+                                    icon: "success",
+                                    image: "",
+                                }).then((willDelete) => {
+                                    fetch('php/backstage_info1_select_expo.php')
+                                    .then(resp =>resp.json())
+                                    .then(resp =>this.datas=resp)
+                                })
+                            }else{
+                                this.$swal({
+                                    title: "刪除失敗",
+                                    icon: "error",
+                                    text: "請檢查欄位",
+                                });
+                            } 
+                        })
                     }
                 })
             },
@@ -517,8 +601,10 @@
                 this.box=null
             },
             editsave(newdata){
-                this.datas[this.row_index]=newdata
                 this.box=null
+                fetch('php/backstage_info1_select_expo.php')
+                .then(resp =>resp.json())
+                .then(resp =>this.datas=resp)
             },
             addclose(){
                 this.box=null
@@ -531,7 +617,7 @@
             }
         },
         template:`
-        <article class="-margin0auto pt-10 table_outer">
+        <article class="-margin0auto pt-10 pb-10 table_outer">
             <button @click="box='backstage_info1_add'" class=" backstage_btn backstage_btn_add mb-15">新增</button>
             <h3 class="bg-color pall-15">{{tablename}}</h3>
             <div class="pt-10 pb-10 bg-in-bgcolor">
