3ea25b9984ce63879e84218818f4878eca8fce6d
SYdni-31 <zemi1211@gmail.com>
2022-04-12 16:35:23 +0800

    feat:新增分頁功能

--- src/js/backstage_index.js
+++ src/js/backstage_index.js
@@ -544,11 +544,16 @@
         props:['tablename'],
         data(){
             return{
-                box:null,
+                box:null, //判斷要打開的彈窗
                 titles:["策展ID", "策展名稱", "活動開始", "狀態", "操作"],
-                datas:'',
-                row_data:null,
-                row_index:null,
+                datas:'', //每一頁的所有資料
+                data_count:'', //資料庫的資料組數
+                pages:1,//總共有的頁數，目前所在的頁數
+                perpage:10, //每頁顯示幾筆
+                inpage:1, //當前頁數
+                centersize:5, // 過多頁數時顯示筆數
+                row_data:null, //被選取那列的資料
+                row_index:null, //被選取那列的序號
             }
         },
         methods:{
@@ -600,27 +605,82 @@
             editclose(){
                 this.box=null
             },
-            editsave(newdata){
+            editsave(){
                 this.box=null
-                fetch('php/backstage_info1_select_expo.php')
-                .then(resp =>resp.json())
-                .then(resp =>this.datas=resp)
+                this.ajax(this.inpage)
             },
             addclose(){
                 this.box=null
             },
             addsave(){
                 this.box=null
-                fetch('php/backstage_info1_select_expo.php')
+                this.ajax(this.inpage)
+            },
+            changepage(page){
+                this.ajax(page)
+            },
+            previouspage(){
+                if(this.inpage>1){
+                    let inpage=this.inpage-1
+                    this.ajax(inpage)
+                }
+            },
+            nextpage(){
+                if(this.inpage<this.pages){
+                    let inpage=this.inpage+1
+                    this.ajax(inpage)
+                }
+                
+            },
+            ajax(inpage){
+                fetch('php/backstage_info1_select_expo.php', {
+                    method: 'POST',
+                    headers:{
+                        'Content-Type': 'application/json'
+                    },
+                    body:JSON.stringify({
+                        inpage: inpage,
+                        perpage: this.perpage,
+                    })
+                })
                 .then(resp =>resp.json())
-                .then(resp =>this.datas=resp)
+                .then(resp => {
+                    this.datas=resp.data
+                    this.data_count=resp.data_count[0][0]
+                    this.pages=Math.ceil(this.data_count/this.perpage)
+                    this.inpage=inpage
+                })
+            }
+        },
+        computed:{
+            centerPages(){
+                let centerPage=this.inpage;
+                if(this.inpage>this.pages-3){
+                    centerPage=this.pages-3
+                }
+                if(this.inpage<4){
+                    centerPage=4
+                }
+                if(this.pages<=this.centersize+2){
+                    const centerArr=[]
+                    for(let i=2; i<this.pages; i++){
+                        centerArr.push(i)
+                    }
+                    return centerArr
+                }else{
+                    const centerArr=[]
+                    for(let i=centerPage-2; i<=centerPage+2; i++){
+                        centerArr.push(i)
+                    }
+                    return centerArr
+                }
             }
         },
         template:`
         <article class="-margin0auto pt-10 pb-10 table_outer">
             <button @click="box='backstage_info1_add'" class=" backstage_btn backstage_btn_add mb-15">新增</button>
             <h3 class="bg-color pall-15">{{tablename}}</h3>
-            <div class="pt-10 pb-10 bg-in-bgcolor">
+            <div class="pall-10 bg-in-bgcolor">
                 <ul class="bg-color -margin0auto backstage-grid title backstage-grid_info1">
                     <li class="bg-color bg-in-secondcolor" v-for="title in titles">{{title}}</li>
                 </ul>
@@ -631,17 +691,37 @@
                     <li class="bg-color bg-in-secondcolor">{{data[4]}}</li>
                     <li class="bg-color bg-in-secondcolor"><div class="backstage_btn_td"><button @click="edit(data, index)" class="backstage_btn backstage_btn_short">修改</button><button @click="del(index)" class="backstage_btn backstage_btn_bad ml-4">刪除</button></div></li>
                 </ul>
-
+                <div class='backstage_pages mt-10'>
+                    <button class='backstage_pages_btn_left mr-2'  @click.stop="previouspage">上一頁</button>
+                    <button @click.prevent='changepage(1)' class='backstage_pages_btn pr-2 pl-2' :class="{'action':inpage==1}">1</button>
+                    <button v-if="pages>centersize+2 && inpage-centersize/2-1>1" class='backstage_pages_btn pr-2 pl-2'>...</button>
+                    <button v-for='(page,index) in centerPages' @click.prevent='changepage(page)' class='backstage_pages_btn pr-2 pl-2' :class="{'action':inpage==page}" :key="index">{{page}}</button>
+                    <button v-if="pages>centersize+2 && inpage+centersize/2+1<pages" class='backstage_pages_btn pr-2 pl-2'>...</button>
+                    <button @click.prevent='changepage(pages)' class='backstage_pages_btn pr-2 pl-2' :class="{'action':inpage==pages}">{{pages}}</button>
+                    <button class='backstage_pages_btn_right ml-2' @click.stop="nextpage">下一頁</button>
+                </div> 
             </div>
             <component :is="box" @editclose="editclose" @editsave="editsave" @addclose="addclose" @addsave="addsave" :row_data="row_data"></component>
         </article>`,
         mounted(){
-            fetch('php/backstage_info1_select_expo.php')
+            fetch('php/backstage_info1_select_expo.php', {
+                method: 'POST',
+                headers:{
+                    'Content-Type': 'application/json'
+                },
+                body:JSON.stringify({
+                    inpage: this.inpage,
+                    perpage: this.perpage,
+                })
+            })
             .then(resp =>resp.json())
-            .then(resp =>this.datas=resp)
+            .then(resp => {
+                this.datas=resp.data
+                this.data_count=resp.data_count[0][0]
+                this.pages=Math.ceil(this.data_count/this.perpage)
+            })
         },
     })
-
 // ========頁面vue========
     const vm=new Vue({
         el:"#page",
