a0cc404447fb3c1db181a3f1c017a9d844385279
gleemarc <marc810824@gmail.com>
2022-04-17 17:06:08 +0800

    feat:新增delete.php/調整select.php

---
+++ src/php/backstage_expo4_delete_live_board.php
@@ -0,0 +1,21 @@
+<?php
+    include("connection.php");
+    $select = json_decode(file_get_contents("php://input"), true);
+
+    $sql = "UPDATE LIVE_BOARD SET STATUS=:STATUS WHERE ID= :ID";
+    $statement = $pdo->prepare($sql);
+    $statement->bindValue(":STATUS", "D");
+    $statement->bindValue(":ID", $select["ID"]);
+    $statement->execute();
+    $resultCount = $statement->rowCount();
+    
+    if($resultCount > 0){
+    $resp["successful"] = true;
+    
+    }else{
+    $resp["successful"] = false;
+    
+    }
+
+    echo json_encode($resp);
+?>
--- src/php/backstage_expo4_select_live_board.php
+++ src/php/backstage_expo4_select_live_board.php
@@ -9,7 +9,7 @@
     
     // 選取第x頁資料(分頁)
     $select2 = json_decode(file_get_contents("php://input"), true);
-    $sql2 = "set @a= concat('select ID, CONTENT, STATUS, LIVE_LIST_ID, COMPANY_ID, GUEST_ID, AGENDA_ID, NAME from view_live_board where ID like' , '\"%', :search_word, '%\"','or CONTENT like', '\"%', :search_word, '%\"', 'or STATUS like', '\"%', :search_word, '%\"', 'or LIVE_LIST_ID like', '\"%', :search_word, '%\"', 'or COMPANY_ID like', '\"%', :search_word, '%\"', 'or GUEST_ID like', '\"%', :search_word, '%\"', 'or GUEST_ID like', '\"%', :search_word, '%\"', 'or AGENDA_ID like', '\"%', :search_word, '%\"', 'or NAME like', '\"%', :search_word, '%\"', ' limit',' ', (:inpage-1)*:perpage, ', ', :perpage)";
+    $sql2 = "set @a= concat('select ID, CONTENT, STATUS, LIVE_LIST_ID, COMPANY_ID, GUEST_ID, AGENDA_ID, NAME from view_live_board where STATUS !=\"D\" and ( ID like' , '\"%', :search_word, '%\"','or CONTENT like', '\"%', :search_word, '%\"', 'or STATUS like', '\"%', :search_word, '%\"', 'or LIVE_LIST_ID like', '\"%', :search_word, '%\"', 'or COMPANY_ID like', '\"%', :search_word, '%\"', 'or GUEST_ID like', '\"%', :search_word, '%\"', 'or GUEST_ID like', '\"%', :search_word, '%\"', 'or AGENDA_ID like', '\"%', :search_word, '%\"', 'or NAME like', '\"%', :search_word, '%\")', ' limit',' ', (:inpage-1)*:perpage, ', ', :perpage)";
     $statement2 = $pdo->prepare($sql2);
     $statement2->bindValue(":inpage", $select2["inpage"], PDO::PARAM_INT);
     $statement2->bindValue(":perpage", $select2["perpage"], PDO::PARAM_INT);
