d03e9248daa9a05d13d03502e2592fe94e6aa938
gleemarc <marc810824@gmail.com>
2022-04-19 18:48:08 +0800

    fix:修正expo4的錯誤與欄位名稱/feat:新增select php:order by time desc

--- src/php/news_select_news.php
+++ src/php/news_select_news.php
@@ -1,7 +1,7 @@
 <?php
     include("connection.php");
     // 選取全部數量
-    $sql="select * from NEWS where STATUS != "D" and OPEN = 1 order by TIME";
+    $sql="select * from NEWS where STATUS != 'D' and OPEN = 1 order by TIME desc";
     $statement=$pdo->prepare($sql);
     $statement->execute();
     $select=$statement->fetchAll(); 
