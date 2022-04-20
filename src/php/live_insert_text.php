154839e3d52c384445f1028ec2a0387c1f8a6706
TFD10506 <TFD10506@gmail.com>
2022-04-20 13:38:10 +0800

    feat: 修改現正直播頁面，改為vue寫法，並讓留言板可搜尋LIVE_BOARD的內容及新增文字時可寫入LIVE_BOARD、自動帶入現在直播時段的廠商資料與聯絡資訊

---
+++ src/php/live_insert_text.php
@@ -0,0 +1,23 @@
+<?php
+    include("connection.php");
+    $select = json_decode(file_get_contents("php://input"), true);
+    // php的欄位全部都大寫
+    $sql = "INSERT INTO LIVE_BOARD(CONTENT, STATUS, COMPANY_ID, GUEST_ID, AGENDA_ID) VALUES (:CONTENT, :STATUS, :COMPANY_ID, :GUEST_ID, (SELECT ID FROM TFD105_G5.AGENDA where date = CURDATE() and HOUR(START_TIME) <= HOUR(now()) and HOUR(END_TIME)>= HOUR(now())))";
+    $statement = $pdo->prepare($sql);
+    $statement->bindValue(":CONTENT", $select["CONTENT"]);
+    $statement->bindValue(":STATUS", $select["STATUS"]);
+    $statement->bindValue(":COMPANY_ID", $select["COMPANY_ID"]);
+    $statement->bindValue(":GUEST_ID", $select["GUEST_ID"]);
+    $statement->execute();
+    $resultCount = $statement->rowCount();
+    
+    if($resultCount > 0){
+    $resp["successful"] = true;
+    
+    }else{
+    $resp["successful"] = false;
+    
+    }
+
+    echo json_encode($resp);
+?>
