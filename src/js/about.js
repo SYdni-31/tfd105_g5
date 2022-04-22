8bdab9d636dfcaf925869ac01da8ff5afe9ad236
jennychiaen <jenny830806@gmail.com>
2022-04-22 18:22:39 +0800

    feat:接收到物件裡的carousel 並增加swiper:js

--- src/js/about.js
+++ src/js/about.js
@@ -67,22 +67,27 @@
 const vm = new Vue({
     el: "#about_main",
     data: {
-        schedules:[
-          
-            
-        ],
-        speaker:"",
+        schedules:[],
+        speakers:[],
         today:"2022-04-10",
         hideiamge:false,
+        timestart:'',
+        carousels:'',
 
     },
     methods: {
-        turn_back(){
-            this.hideiamge = !this.hideiamge
-        }
+        turn_back(index){
+            // console.log(e.currentTarget);
+            // e.currentTarget.classList.toggle('-hide')
+            // this.hideiamge = !this.hideiamge
+            document.getElementsByClassName('about_speaker_intro')[index].classList.toggle('-hide')
+            document.getElementsByClassName('about_hoverText')[index].classList.toggle('-hide')
+
+        },
+    
     },
     mounted(){
-        fetch("php/about_select_about.php",{
+        fetch("php/about_select_agenda.php",{
             method: 'POST', //傳到php
             headers: {
                 'Content-Type': 'application/json'
@@ -93,8 +98,54 @@ const vm = new Vue({
         }).then(resp => resp.json())//接收
         .then(body => {
             console.log(body);
-            this.schedules=body
+            console.log(body['agenda']);
+            console.log(body['carousel']);
+            this.schedules=body['agenda'];
+            this.carousels=body['carousel'];
+            // this.schedules=body;
+            for(let i=0; i<body['agenda'].length; i++){
+            // this.timestart=body[i].START_TIME.slice(0,-3);
+                if(body['agenda'][i].PHOTO !=null &&body['agenda'][i].PHOTO !=""){
+                    this.speakers.push(body['agenda'][i])
+                }
+            }
+
+           
         })
+        var swiper = new Swiper(".mySwiper", {
+              slidesPerView: 7, //一頁幾個
+              spaceBetween: 50,  //照片間格
+              loop: true,
+              scrollbar: {
+                el: ".swiper-scrollbar",
+                hide: false,
+                draggable: true,
+                dragSize: 10,
+            
+              },
+              breakpoints: {
+                320: {
+                  slidesPerView: 4,
+                  spaceBetween: 20,
+                },
+                375: {
+                  slidesPerView: 4,
+                  spaceBetween: 20,
+                },
+                480: {
+                  slidesPerView: 4,
+                  spaceBetween: 20,
+                },
+                768: {
+                  slidesPerView: 5,
+                  spaceBetween: 20,
+                },
+                1024: {
+                  slidesPerView: 7,
+                  spaceBetween: 50,
+                },
+              }
+            });
 
 
     },
