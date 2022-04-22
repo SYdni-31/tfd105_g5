d1f3732635e13693f0cba297eaeec8cc54df6aa8
gleemarc <marc810824@gmail.com>
2022-04-22 14:29:42 +0800

    feat:調整js函數順序至new vue/fix:修正php的錯誤

--- src/php/companyback_select_tech.php
+++ src/php/companyback_select_tech.php
@@ -1,11 +1,13 @@
 <?php
     include("connection.php");
+    //PHP接收 JSON POST
+    $select = json_decode(file_get_contents("php://input"), true);
     // 選取全部數量
-    $sql="select NAME, LINK from TECH where STATUS != 'D'";
+    $sql="select NAME, LINK from TECH where STATUS != 'D' and COMPANY_INFO_ID=:id";  
     $statement=$pdo->prepare($sql);
+    $statement->bindValue(":id", $select["company_info_id"]);
     $statement->execute();
     $select=$statement->fetchAll(); 
-    //PHP接收 JSON POST
 
     if ($select != null){
         echo json_encode($select);
