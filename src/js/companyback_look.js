f05528bb5e47662e81b3a3656ad8b42201cc1802
SYdni-31 <zemi1211@gmail.com>
2022-04-23 21:26:43 +0800

    feat:新增company_look按鈕更新上架功能

--- src/js/companyback_look.js
+++ src/js/companyback_look.js
@@ -10,6 +10,36 @@ const vm = new Vue({
         little(i){
             this.little_page=i
         },
+        onboard(){
+            fetch('php/companyback_look_update_company_info.php',{
+                method: 'POST',
+                headers:{
+                    'Content-Type': 'application/json'
+                },
+                body:JSON.stringify({
+                    ID:this.info_id,
+                })
+            }).then(resp => resp.json())
+            .then(resp => {
+                let {successful} =resp
+                if(successful){
+                    this.$swal({
+                        title: "上架成功",
+                        icon: "success",
+                        image: "",
+                    }).then((willDelete) => {
+                        document.location.href='companyback.html'
+                    })
+
+                } else {
+                    this.$swal({
+                        title: "上架失敗",
+                        icon: "error",
+                        text: "請重新確認資料是否正確",
+                    });
+                }
+            })
+        },
     },
     created(){
         fetch('php/companyback_look_select_company_info.php',{
@@ -22,7 +52,7 @@ const vm = new Vue({
             })
         }).then(resp => resp.json())
         .then(resp => {
-            this.looks=resp.rooms[0]
+            this.looks=resp.rooms
             this.techs=resp.tech
         })
 
