e96b54367c062f91a89080559f4c08583498bbe8
jennychiaen <jenny830806@gmail.com>
2022-04-24 14:11:40 +0800

    feat:新增about.php  slogan日期活動

--- src/php/about_select_agenda.php
+++ src/php/about_select_agenda.php
@@ -1,6 +1,6 @@
 <?php
     include("connection.php");
-    // 選取全部數量
+    //會議
     $select = json_decode(file_get_contents("php://input"), true); 
     $sql="select * from agenda where DATE = :today
     union
@@ -12,15 +12,24 @@
     $select=$statement->fetchAll(); 
     //PHP接收 JSON POST
 
+    //輪播照片
     $sql2="select LOGO from view_company_info;";
     $statement2 = $pdo->prepare($sql2);
     $statement2->execute();
     $select2= $statement2->fetchAll();
 
 
+    //slogan 日期活動
+    $sql3="select NAME,START_TIME,END_TIME,INTRODUCE from tfd105_g5.expo where OPEN ='進行中';";
+    $statement3 = $pdo->prepare($sql3);
+    $statement3->execute();
+    $select3= $statement3->fetchAll();
+
+
     if ($select != null){
         $folder['agenda']=$select;
         $folder['carousel']=$select2;
+        $folder['expo']=$select3;
         echo json_encode($folder);
     };
 ?>
