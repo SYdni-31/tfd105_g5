2ecdceb22c187de8f15f14d9d8cbcaff61660e53
TFD10506 <TFD10506@gmail.com>
2022-04-24 18:40:12 +0800

    feat: 修改現正直播頁面，新增判斷是否有session，無session不可以留言，並將session的值帶入留言者的姓名

--- src/php/live_insert_live_board.php
+++ src/php/live_insert_live_board.php
@@ -2,7 +2,7 @@
     include("connection.php");
     $select = json_decode(file_get_contents("php://input"), true);
     // php的欄位全部都大寫
-    $sql = "INSERT INTO LIVE_BOARD(CONTENT, STATUS, COMPANY_ID, GUEST_ID, AGENDA_ID) VALUES (:CONTENT, :STATUS, :COMPANY_ID, :GUEST_ID, (SELECT ID FROM TFD105_G5.AGENDA where date = CURDATE() and HOUR(START_TIME) <= HOUR(now()) and HOUR(END_TIME)>= HOUR(now())))";
+    $sql = "INSERT INTO LIVE_BOARD(CONTENT, STATUS, COMPANY_ID, GUEST_ID, AGENDA_ID) VALUES (:CONTENT, :STATUS, :COMPANY_ID, :GUEST_ID, (SELECT ID FROM AGENDA where date = CURDATE() and HOUR(START_TIME) = HOUR(now()) and HOUR(END_TIME)>= HOUR(now())))";
     $statement = $pdo->prepare($sql);
     $statement->bindValue(":CONTENT", $select["CONTENT"]);
     $statement->bindValue(":STATUS", $select["STATUS"]);
--- src/php/live_select_live_board.php
+++ src/php/live_select_live_board.php
@@ -2,7 +2,7 @@
     include("connection.php");
     // 選取全部數量
     // $select = json_decode(file_get_contents("php://input"), true);      // 只有變數可以修改
-    $sql= "SELECT  NAME ,CONTENT FROM view_live_board where AGENDA_ID = (SELECT ID FROM TFD105_G5.AGENDA where date = CURDATE() and HOUR(START_TIME) <= HOUR(now()) and HOUR(END_TIME)>= HOUR(now())) ";
+    $sql= "SELECT  NAME ,CONTENT FROM view_live_board where AGENDA_ID = (SELECT ID FROM AGENDA where date = CURDATE() and HOUR(START_TIME) = HOUR(now()) and HOUR(END_TIME)>= HOUR(now()) limit 1) ";
     $statement=$pdo->prepare($sql);
     $statement->execute();
     $select=$statement->fetchAll(); 
