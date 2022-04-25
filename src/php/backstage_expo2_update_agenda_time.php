36b61851eaad5170705542827ec8f60d89530500
TFD10506 <TFD10506@gmail.com>
2022-04-25 11:49:55 +0800

    fix:修正後台[大會時段]修改時點擊儲存不成功問題

--- src/php/backstage_expo2_update_agenda_time.php
+++ src/php/backstage_expo2_update_agenda_time.php
@@ -1,8 +1,7 @@
 <?php
     include("connection.php");
     $select = json_decode(file_get_contents("php://input"), true);
-    print_r($select);
-    // exit();
+
     //php的欄位全部都大寫
     $sql = "UPDATE AGENDA_TIME SET START_TIME=:START_TIME, END_TIME=:END_TIME, STATUS=:STATUS , OPEN=:OPEN WHERE ID= :ID";
     $statement = $pdo->prepare($sql);
@@ -13,15 +12,11 @@
     $statement->bindValue(":OPEN", $select["OPEN"] , PDO::PARAM_INT);
     $statement->execute();
     $resultCount = $statement->rowCount();
-    print_r($resultCount);
+
     if($resultCount > 0){
-    $resp["successful"] = true;
-    
+        $resp["successful"] = true;
     }else{
-    $resp["successful"] = false;
-
-    
+        $resp["successful"] = false;
     }
-
     echo json_encode($resp);
 ?>
