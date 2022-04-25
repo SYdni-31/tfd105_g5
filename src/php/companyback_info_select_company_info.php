3b532f50adb29d165dfcd2ac53b025b9606678db
Brandon880427 <a22540051@yahoo.com.tw>
2022-04-25 20:33:10 +0800

    fix更替中英文字順序

--- src/php/companyback_info_select_company_info.php
+++ src/php/companyback_info_select_company_info.php
@@ -1,7 +1,7 @@
 <?php
 include("connection.php");
 $select = json_decode(file_get_contents("php://input"), true);
-$sql = "SELECT * FROM view_company_info where ID = :ID";
+$sql = "SELECT * FROM view_company_info where COMPANY_ID = :ID";
 $statement = $pdo->prepare($sql); 
 $statement->bindValue(":ID",$select["ID"]);
 $statement->execute();
